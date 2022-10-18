174f1fc571153a2bf4a69ac0ad57aa715bc56696
lydiavernon <111576826+lydiavernon@users.noreply.github.com>
2022-10-18 17:40:34 +0100

    addAPI getwarehouses and render

---
+++ src/Components/WarehouseItem/WarehouseItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const WarehouseItem = ({ warehouse }) => {
+  return (
+    <article className="card">
+      <div className="card__text-wrapper">
+        <div className="card__left">
+          <h5 className="card__label">Warehouse</h5>
+          <p className="card__name">{warehouse.name}</p>
+          <h5 className="card__label">Address</h5>
+          <p className="card__address">
+            {warehouse.address}, {warehouse.city}, {warehouse.country}
+          </p>
+        </div>
+        <div className="card__right">
+          <h5 className="card__label">Contact Name</h5>
+          <p className="card__contact-name">{warehouse.contact.name}</p>
+          <h5 className="card__label">Contact Information</h5>
+          <p className="card__contact-phone">{warehouse.contact.phone}</p>
+          <p className="card__contact-email">{warehouse.contact.email}</p>
+        </div>
+      </div>
+      <div>
+        <button className="card__delete-button"></button>
+        <button className="card__edit-button"></button>
+      </div>
+    </article>
+  );
+};
+
+export default WarehouseItem;
