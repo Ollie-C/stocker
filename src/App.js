38be96ff5218926e43145d8b4f4f374a4840b5a1
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-10-18 20:41:42 +0100

    modal proto works 2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import Modal from "./Components/Modal/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  //set show modal first to false
+  const hideModal = () => {
+    setShowModal(false);
+  };
+
+  //select product to open a model
+  const handleSelectedProduct = (e, product) => {
+    setSelectedProduct(product);
+
+    setShowModal(true);
+  };
+
   return (
     <>
-      <Modal />
+      <div>
+        <button onClick={handleSelectedProduct}>click Modal</button>
+      </div>
+      {showModal ? (
+        <Modal
+          handleSelectedProduct={handleSelectedProduct}
+          hideModal={hideModal}
+        />
+      ) : null}
     </>
   );
 }
--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "./Modal.scss";
-import close from "../../assets/Icons/close-24px.svg";
+import closeBtn from "../../assets/Icons/close-24px.svg";
+import { Link } from "react-router-dom";
 
-const Modal = () => {
+const Modal = ({ hideModal, handleSelectedProduct }) => {
   return (
     <section className="modal">
-      <div className="modal__cross-icon">
-        <img src={close} alt="" />
-      </div>
-      <div className="modal__title-container">
-        <h1 className="modal__title-container">Delete Washington warehouse</h1>
-      </div>
-      <div className="modal__confirmation">
-        <p className="modal__confirmation-paragraph">
-          Please confirm that you’d like to delete the Washington from the list
-          of warehouses. You won’t be able to undo this action.
-        </p>
-      </div>
-      <div className="modal__buttons">
-        <button className="modal__button-cancel">cancel</button>
-        <button className="modal__button-delete">delete</button>
+      <div className="modal__card">
+        <div className="modal__card-within">
+          {/* <Link to="/"> */}
+          <img className="modal__cross-btn" src={closeBtn} alt="" />
+          {/* </Link> */}
+          <div className="modal__title-paragraph-container">
+            <h1 className="modal__title">Delete Washington warehouse</h1>
+            <p className="modal__confirmation-paragraph">
+              Please confirm that you’d like to delete the Washington from the
+              list of warehouses. You won’t be able to undo this action.
+            </p>
+          </div>
+          <div className="modal__buttons">
+            <button onClick={hideModal} className="modal__button-cancel">
+              cancel
+            </button>
+            <button className="modal__button-delete">delete</button>
+          </div>
+        </div>
       </div>
     </section>
   );
