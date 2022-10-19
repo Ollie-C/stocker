b3ed79bee20c01ec61d005098b31c335ba1b684e
Ollie Cross <iio.olivercross@gmail.com>
2022-10-19 14:30:24 +0100

    Added basic functionality

--- src/Components/AddWarehouse/AddWarehouse.js
+++ src/Components/AddWarehouse/AddWarehouse.js
@@ -1,37 +1,74 @@
 //styles
 import "./AddWarehouse.scss";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import backIcon from "../../assets/Icons/arrow_back-24px.svg";
 
 const AddWarehouse = () => {
+  //   const navigate = useNavigate();
+  //   const [name, setName] = useState("");
+  //   const [address, setAddress] = useState("");
+  //   const [city, setcity] = useState("");
+  //   const [country, setcountry] = useState("");
+  //   const [contactName, setcontactName] = useState("");
+  //   const [position, setposition] = useState("");
+  //   //   const [phone, setphone] = useState("");
+  //   //   const [email, setemail] = useState("");
+
+  //   const addWarehouse = async (e) => {
+  //     e.preventDefault();
+  //     const { data } = await axios
+  //       .post("http://localhost:8080/warehouses", {
+  //         name: name,
+  //         address: address,
+  //         city: city,
+  //         country: country,
+  //         contact: {
+  //           contactName: contactName,
+  //           position: position,
+  //           phone: phone,
+  //           email: email,
+  //         },
+  //       })
+  //       .catch((error) => {
+  //         alert(error.response.statusText);
+  //         console.log(error.response);
+  //       });
+  //   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //   addWarehouse();
+    //   alert("Success.");
+    //   navigate("/");
+  };
+
   return (
     <main className="main">
       <section className="header">
         <img className="header__icon" src={backIcon} alt="back-button" />
         <h1 className="header__title">Add New Warehouse</h1>
       </section>
-
-      <form className="form" id="addWarehouseForm">
+      <form className="form" id="addWarehouseForm" onSubmit={submitHandler}>
         <div className="form__fields">
           <h2 className="form__title">Warehouse Details</h2>
-          <label htmlFor="warehouseName" className="form__label">
+          <label htmlFor="name" className="form__label">
             Warehouse Name
           </label>
           <input
             type="text"
             className="form__input"
             placeholder="Warehouse Name"
-            name="warehouseName"
+            name="name"
           />
-          <label htmlFor="warehouseAddress" className="form__label">
+          <label htmlFor="address" className="form__label">
             Street Address
           </label>
           <input
             type="text"
             className="form__input"
             placeholder="Street Address"
-            name="warehouseAddress"
+            name="address"
           />
           <label htmlFor="city" className="form__label">
             City
@@ -94,7 +131,9 @@ const AddWarehouse = () => {
       </form>
 
       <section className="form__buttons">
-        <button className="form__button">Cancel</button>
+        <button className="form__button" /*onClick={() => navigate(-1)}*/>
+          Cancel
+        </button>
         <button
           className="form__button form__button--blue"
           form="addWarehouseForm"
