a0a1823440709d0ed739acd224242993d0d0b88a
lydiavernon <111576826+lydiavernon@users.noreply.github.com>
2022-10-24 16:57:01 +0100

    add style to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,11 @@
+import "./Footer.scss";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <p className="footer__text">© InStock Inc. All Rights Reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
